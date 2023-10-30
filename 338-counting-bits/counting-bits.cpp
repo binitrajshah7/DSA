ef3aa0ca11c587342810b427442aacf382d24c95
Binit Kumar Shah <binitrajshah931@gmail.com>
2023-10-30 17:55:13 +0530

    Time: 3 ms (83.26%) | Memory: 8.1 MB (51.23%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
+        // we will initialise dp of array n+1
         vector<int> dp(n + 1, 0);
+        // as we move a element right it will be smaller so we look for number of set bits in smaller element
         dp[0] = 0;
         for (int i = 1; i <= n; i++){
             int key = i;
