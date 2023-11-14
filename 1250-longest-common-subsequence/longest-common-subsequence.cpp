278c137fceb16dab75bcfd05c2ac72673795d539
Binit Kumar Shah <binitrajshah931@gmail.com>
2023-11-14 17:14:21 +0530

    Time: 46 ms (69.53%) | Memory: 24.7 MB (56.93%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-    // Intuition: 
-    // Brute Force: Generate all the subsequence [recursive or power set method] and check for every 
-    // subsequence from text1 with subsequence of text2
-
-    // Better Approach: We generate subsequence & keep on checking for the longest on the go
-    // We traverse from back and check whether current index of t1 and t2 matches or not
-    int solve(string &text1, int index1, string &text2, int index2, vector<vector<int>> &dp){
-        if(index1 < 0 || index2 < 0)
-            return 0;
-
-        if(dp[index1][index2] != -1)
-            return dp[index1][index2];
-
-        // Condition 1: When index of both t1 and t2 matches then we move both pointers one behind [MATCH] 
-        if(text1[index1] == text2[index2])
-            return dp[index1][index2] = 1+solve(text1, index1-1, text2, index2-1, dp);
-
-        // we move 1 index from t1 and call recursively and 1 index from t2 and call again [NOT MATCH]
-        // we take maximum of of both recursive call
-        return dp[index1][index2] = max(solve(text1, index1-1, text2, index2, dp), solve(text1, index1, text2, index2-1, dp)); 
-    }
-
+    // Since our i and j were going to -1 in our recursive call so we right shifted our index by 1 so base case is handled
     int longestCommonSubsequence(string text1, string text2) {
-        int l1 = text1.length();
-        int l2 = text2.length();
+        int r = text1.length();
+        int c = text2.length();
+        vector<vector<int>> dp(r+1, vector<int>(c+1, 0));
 
-        vector<vector<int>> dp(l1, vector<int>(l2, -1));
-        return solve(text1, l1-1, text2, l2-1, dp);
+        // Base Case if(i==0 || j==0 return 0;)
+        for(int j=0; j<c; j++)
+            dp[0][j] = 0;
+
+        for(int i=0; i<r; i++)
+            dp[i][0] = 0;
+
+        // Write down changing parameters in Bottom up approach
+        for(int i=1; i<=r; i++){
+            for(int j=1; j<=c; j++){
+                // copy the recurrence
+                if(text1[i-1] == text2[j-1])
+                    dp[i][j] = 1+dp[i-1][j-1];
+                else
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[r][c];
     }
 };
