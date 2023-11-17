32b2685256481eab343530748ff1111cce39f87d
Binit Kumar Shah <binitrajshah931@gmail.com>
2023-11-17 15:54:45 +0530

    Time: 3 ms (73.81%) | Memory: 12.4 MB (30.66%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     // Carry Forward from Buy & Sell Stock 2
-    int solve(vector<int> &prices, int index, bool buy, vector<vector<int>> &dp){
-        if(index >= prices.size())
-            return 0;
-
-        if(dp[index][buy] != -1)
-            return dp[index][buy];
-
-        int profit = 0;
-        if(buy){
-            profit = max(-prices[index] + solve(prices, index+1, 0, dp), solve(prices, index+1, 1, dp));
-        }   
-        else{
-            // After selling stock moving to 2 index ahead after cooldown period
-            profit = max(prices[index] + solve(prices, index+2, 1, dp), solve(prices, index+1, 0, dp));
-        }
-        return dp[index][buy] = profit;
-    }
-
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int> (2, -1));
-        return solve(prices, 0, 1, dp);
+        vector<vector<int>> dp(n+2, vector<int> (2, 0));
+
+        for(int index=n-1; index>=0; index--){
+            for(int buy=0; buy<=1; buy++){
+                int profit = 0;
+                if(buy){
+                    profit = max(-prices[index] + dp[index+1][0], dp[index+1][1]);
+                }   
+                else{
+                    // After selling stock moving to 2 index ahead after cooldown period
+                    profit = max(prices[index] + dp[index+2][1],dp[index+1][0]);
+                }
+                dp[index][buy] = profit;
+            }
+        }
+
+        return dp[0][1];
     }
 };
