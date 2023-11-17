0516868a5fd32602a14378bda6dabd2fd714bb7a
Binit Kumar Shah <binitrajshah931@gmail.com>
2023-11-17 11:32:17 +0530

    Time: 12 ms (13.89%) | Memory: 15.4 MB (24.52%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,31 +4,29 @@ public:
     // We keep a flag whether at a particular index we are allowed to buy or not
     // if we buy at current index we retrict to buy in next index so next index will have two options either to sell or not sell
 
-    int solve(vector<int> &prices, int index, bool buy, vector<vector<int>> &dp){
-        if(index == prices.size())
-            return 0;
-
-        if(dp[index][buy] != -1)
-            return dp[index][buy];
-        
-        int profit = 0;
-        if(buy){
-            profit = max((-prices[index] + solve(prices, index+1, 0, dp)), 
-                        solve(prices, index+1, 1, dp));
-        }
-        else{
-            profit = max((prices[index] + solve(prices, index+1, 1, dp)),
-                        solve(prices, index+1, 0, dp));
-        }
-        return dp[index][buy] = profit;
-    }
-
     int maxProfit(vector<int>& prices) {
         // First index will always have permission to buy
-        
         // As buy can have only two options 0, 1 so our dp will be dp[n] * [2]
+        int n = prices.size();
+        vector<vector<int>> dp(prices.size()+1, vector<int> (2, 0));
 
-        vector<vector<int>> dp(prices.size(), vector<int> (2, -1));
-        return solve(prices, 0, 1, dp);    
+        // Base case when index=prices.size() return 0;
+        dp[n][0] = 0;
+        dp[n][1] = 0;
+
+        // write changing parameters in reverse order bottom up
+        for(int index=n-1; index>=0; index--){
+            for(int buy=0; buy<=1; buy++){
+                int profit = 0;
+                if(buy){
+                    profit = max(-prices[index] + dp[index+1][0], dp[index+1][1]);
+                }
+                else{
+                    profit = max(prices[index] + dp[index+1][1], dp[index+1][0]);
+                }
+                dp[index][buy] = profit;
+            }
+        }
+        return dp[0][1];    
     }
 };
