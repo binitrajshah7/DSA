954091ddd4bc7a77775d1e1435bb6fd237bcffd4
Binit Kumar Shah <binitrajshah931@gmail.com>
2023-11-17 14:45:58 +0530

    Time: 12 ms (66.91%) | Memory: 14.8 MB (34.95%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,35 +1,47 @@
 class Solution {
 public:
-    // Carry forward from buy and stock III
-    // Intuition same as Buy & Sell Stock II but we keep an additional check for transaction capacity
-    int solve(vector<int> &prices, int index, bool buy, int capacity, vector<vector<vector<int>>> &dp){
-        if(capacity == 0 || index == prices.size())
-            return 0;
-        
-        if(dp[index][buy][capacity] != -1)
-            return dp[index][buy][capacity];
-
-        int profit = 0;
-        // buy or not buy
-        if(buy){
-            // if buy then we invested money so we take profit as negative not buy then we simply go to next index with can buy as true
-            profit = max(-prices[index] + solve(prices, index+1, 0, capacity, dp),
-                        solve(prices, index+1, 1, capacity, dp));
-        }   
-        else{
-            // if sell then we complete our 1 transaction so we reduce the capacity by 1
-            profit = max(prices[index] + solve(prices, index+1, 1, capacity-1, dp),
-                        solve(prices, index+1, 0, capacity, dp));
-        }
-        return dp[index][buy][capacity]=profit;
-    }
+    
 
     int maxProfit(int k, vector<int>& prices) {
         // solve(prices, index, can_buy, capacity)
         int n = prices.size();
 
         // dp[n] * [2] * [k]
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k+1, -1)));
-        return solve(prices, 0, 1, k, dp);
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1, 0)));
+
+        /*
+        // If index is anything and buy is anything but cap is 0 then no transaction so profit 0
+        for(int i=0; i<n; i++){
+            for(int buy=0; buy<=1; buy++){
+                dp[i][buy][0] = 0;
+            }
+        }
+
+        // For any index out of bound profit will be 0
+        for(int buy=0; buy<=1; buy++){
+            for(int cap=0; cap<=2; cap++){
+                dp[n][buy][cap] = 0;
+            }
+        }
+        */
+        // As we have initialised our dp to 0 itself so writing base cases doesn't makes sense just for reference
+        for(int index=n-1; index>=0; index--){
+            for(int buy=0; buy<=1; buy++){
+                for(int capacity=1; capacity<=k; capacity++){
+                    int profit = 0;
+                    // buy or not buy
+                    if(buy){
+                        // if buy then we invested money so we take profit as negative not buy then we simply go to next index with can buy as true
+                        profit = max(-prices[index] + dp[index+1][0][capacity], dp[index+1][1][capacity]);
+                    }   
+                    else{
+                        // if sell then we complete our 1 transaction so we reduce the capacity by 1
+                        profit = max(prices[index] + dp[index+1][1][capacity-1], dp[index+1][0][capacity]);
+                    }
+                    dp[index][buy][capacity]=profit;
+                }
+            }
+        }
+        return dp[0][1][k];
     }
 };
