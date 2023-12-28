dfe6b6a042a80d4445802998f145fd8fc3dd5336
Binit Kumar Shah <binitrajshah931@gmail.com>
2023-12-28 09:20:52 +0530

    Time: 91 ms (5.23%) | Memory: 8.8 MB (53.54%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    // Intuition: Here we consider all the possibility after rolling a dice
     int MOD = 1e9 + 7;
-    int solve(int n, int k, int target, vector<vector<int>> &dp){
-        // when no dice left to be rolled
-        if(n == 0){
-            // if target == 0 then return 1 else 0
-            return target==0? 1: 0;
-        }
-
-        if(dp[n][target] != -1)
-            return (dp[n][target])%MOD;
-
-        int result = 0;
-        // all the possibility after rolling a dice
-        // this step implies we have rolled a dice one time so remaining dice = n-1
-        // and target = target-dice_num
-        for(int dice_num=1; dice_num<=k; dice_num++){
-            if(dice_num <= target)
-                result = (result + solve(n-1, k, target - dice_num, dp))%MOD;
-            else
-                break;
-        }
-        return dp[n][target] = result;
-    }
-
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>> dp(n+1, vector<int> (target+1, -1));
-        return solve(n, k, target, dp);
+        // bottom up approach
+        vector<vector<int>> dp(n+1, vector<int> (target+1, 0));
+
+        // base case when no dice left and target == 0
+        dp[0][0] = 1;
+
+        // number of dice
+        for(int i=1; i<=n; i++){
+            // target
+            for(int j=1; j<=target; j++){
+                // from recurrence
+                int result = 0;
+                for(int dice_num=1; dice_num<=k; dice_num++){
+                    if(dice_num <= j)
+                        result = (result + dp[i-1][j - dice_num])%MOD;
+                }
+                dp[i][j] = result;
+            }
+        }
+
+        return (dp[n][target])%MOD;
     }
 };
