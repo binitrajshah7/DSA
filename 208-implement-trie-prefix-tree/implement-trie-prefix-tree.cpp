f7e89998544e701657af1866b9f4b0180515123f
Binit Kumar Shah <binitrajshah931@gmail.com>
2023-12-31 19:25:41 +0530

    Time: 46 ms (81.07%) | Memory: 44 MB (37.20%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -1,28 +1,78 @@
+// A node of trie contains can contain 26 character and each character can have link/reference to next node
+// flag here signifies the end of a word in a trie
+struct Node{
+    Node * links[26];
+    bool flag = false;
+
+    bool contains_key(char ch){
+        return (links[ch-'a'] != NULL);
+    }
+        
+    void put(char ch, Node* node){
+        links[ch-'a'] = node;
+    }
+        
+    Node * get(char ch) {
+        return links[ch - 'a'];
+    }
+
+    void set_end(){
+        flag = true;
+    }
+
+    bool is_end(){
+        return flag;
+    }
+};
+
 class Trie {
+private: 
+    Node* root;
 public:
-    unordered_map<string, int> mpp, mp;
     Trie() {
+        root = new Node();
     }
     
+    /* Inserts a word into the trie. */
+    // Time Complexity = O(n)
     void insert(string word) {
-        mpp[word]++;
-        string s;
-        for(int i=0; i<word.length(); i++){
-            s += word[i];
-            mp[s]++;
+        Node* node = root;
+
+        for(int i=0; i<word.size(); i++){
+            if(!node->contains_key(word[i])){
+                node->put(word[i], new Node());
+            }
+
+            // moves to the reference trie
+            node = node->get(word[i]);
         }
+        node->set_end();
     }
     
+    // Time complexity = O(n) n = size of word
     bool search(string word) {
-        if(mpp.find(word) != mpp.end())
-            return true;
-        return false;
+        Node *node = root;
+        for(int i=0; i<word.size(); i++){
+            if(node->contains_key(word[i]))
+                node = node->get(word[i]);
+            else    
+                return false;
+        }
+        // verifying if this is the end or not
+        return node->is_end();
     }
     
+    // Time complexity = O(n) n = size of word
     bool startsWith(string prefix) {
-        if(mp[prefix])
-            return true;
-        return false;        
+        Node* node = root;
+
+        for(int i=0; i<prefix.size(); i++){
+            if(!node->contains_key(prefix[i]))
+                return false;
+
+            node = node->get(prefix[i]);
+        }
+        return true;
     }
 };
 
