e94061c69c87c92f541d062037f0d426a9d96817
Binit Kumar Shah <binitrajshah931@gmail.com>
2024-02-08 22:49:01 +0530

    Time: 184 ms (27.39%) | Memory: 11.2 MB (25.84%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    // Intuition is same like coin change problem where we can decide whether to pick a coin or not
+    // since it has to be subtracted from perfect square number so our possible coins can be i = 1, 2, 3, ...
+    // we just have to keep a check in place that i*i < n then only we can consider picking or not picking a number
+    // for perfect squares
     int get_min_count(int n, int i, vector<int> &dp){
         if (n == 0)     
             return 0;
@@ -8,7 +12,9 @@ public:
             return dp[n];
 
         if (i*i <= n){
+            // as we can pick a coin multiple times so we decrease our target and keep pointer to same
             int pick = 1+get_min_count(n-i*i, i, dp);
+            // as we don't pick we will move forward in index keeping target same
             int not_pick = get_min_count(n, i+1, dp);
             return dp[n] = min(pick, not_pick);
         }
