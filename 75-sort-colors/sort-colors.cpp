d0cba6e301f515ecc3fe63fe31ef04dc32b8002c
Binit Kumar Shah <binitrajshah931@gmail.com>
2024-06-13 00:04:02 +0530

    Time: 5 ms (17.05%) | Memory: 9.9 MB (5.81%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // we split 3 windows low, mid, high
-        int low=0, mid=0, high=nums.size()-1;
-        while(mid<=high){
-            // mid is our main pointer that increases while going further
-            switch(nums[mid]){
-                // if we find 0 i.e we will increase low window size and mid window size
-                case 0:
-                    swap(nums[low++], nums[mid++]);
-                    break;
-                // if we find 1 simply increase only mid window size
-                case 1:
-                    mid++;
-                    break;
-                // if we find 2 we will swap 2 with last window of high and increase 2's window size
-                case 2:
-                    swap(nums[mid], nums[high--]);
-                    break;
+        int n = nums.size();
+        int p1 = 0, p2 = n-1;
+        for(int i = 0;i<=p2; i++)
+        {
+            if(nums[i] == 0)
+            {
+                swap(nums[i], nums[p1]);
+                p1++;
             }
+            else if(nums[i] == 2 )
+            {
+                swap(nums[i], nums[p2]);
+                p2--;
+                i--;
+            }     
         }
-        
     }
 };
